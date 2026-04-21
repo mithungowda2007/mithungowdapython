fb9b01ccbeacbd2f4422d43e485ee94b23ddfd58
mithungowda2007 <msmithungowda@gmail.com>
2026-04-21 08:52:21 +0530

    Add files via upload

---
+++ python2.2class.py
@@ -0,0 +1,6 @@
+
+num=[12345]
+lang=['python','c','java','php']
+print(num + lang)
+print(num * 2)
+print(lang [2])
---
+++ python2.3class.py
@@ -0,0 +1,5 @@
+
+lang=['python','c','java','php']
+print("The list items are \n")
+for i in lang :
+   print(i)
